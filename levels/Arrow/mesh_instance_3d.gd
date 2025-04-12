b5ad077cbff9df8a5179e30468f34f0825268a29
Michael Foust <ragnarokerxiii@gmail.com>
2025-04-11 23:42:38 -0400

    feat: movement based on mouse movement in 3d. Prone to Crashes

---
+++ levels/Arrow/mesh_instance_3d.gd
@@ -0,0 +1,11 @@
+extends MeshInstance3D
+
+func _process(delta):
+	var target_plane = Plane(Vector3(0,0,-1), position.z)
+	var ray_len = 1000
+	var mp = get_viewport().get_mouse_position()
+	var from = $"../Camera3D".project_ray_origin(mp)
+	var to = from + $"../Camera3D".project_ray_normal(mp) * ray_len
+	var cursor_pos = target_plane.intersects_ray(from, to)
+	$".".look_at(cursor_pos, Vector3.MODEL_FRONT, 0)
+	pass
