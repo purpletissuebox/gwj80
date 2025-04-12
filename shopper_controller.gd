2bb78fe1423cd24cf976d4d81be4b5e20a3000fc
Harrison Wine <harrisonmwine@protonmail.com>
2025-04-11 21:17:08 -0400

    initial scene and script for mob
    script only contains idears

---
+++ shopper_controller.gd
@@ -0,0 +1,13 @@
+class_name ShopperController extends Node
+
+# TODO.hw - implement navigation pathing for shopper mobs
+# will want a couple types: Grannies, Kiddos, Shelfstockers
+# Grannies - meander to item spawn points, blocking the isle, stop and peruse items
+# Shelfstockers - move at a moderate pace to item spawn points, takes direct paths, lingers to restock
+# Kiddos - spawn of Satan, will run around with no concern for themselves or others
+
+# TODO.hw - add a Manager control scheme that wanders the store and if seeing the player hunts
+# them down to kick them out.
+
+
+# TODO.hw - upon collision emit death
