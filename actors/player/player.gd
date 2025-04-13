1cc7a2210f0e20c437e7b2aa6803c9839e362f9f
purple tissue box <dirksenmatthew1@gmail.com>
2025-04-12 23:12:58 -0400

    quick and dirty function to test the sounds

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -16,6 +16,51 @@ class_name ShoppingCart extends RigidBody3D
 @export var front_wheels:Array[RayCast3D]
 
 var desired_thrust:float = 0
+var prev_vel:Vector3 = Vector3.ZERO
+var engine_sound:AudioStreamPlayer = null
+var drift_sound:AudioStreamPlayer = null
+var sound_cooldown:float = 0.0
+
+func _ready():
+	AudioDriver.play_bgm("res://levels/test_shop/bgm.mp3", 0)
+
+func _physics_process(delta):
+	if self.sound_cooldown > 0:
+		self.sound_cooldown -= delta
+		self.prev_vel = self.linear_velocity
+		return
+	
+	if (self.linear_velocity - self.prev_vel).length() > 30.0:
+		AudioDriver.play_sfx("res://actors/player/sounds/crash.mp3")
+		cancelSound(1)
+		cancelSound(0)
+		self.sound_cooldown = 1.2
+		return
+	
+	if self.angular_velocity.length() > 3.0 or desired_thrust * self.linear_velocity.dot(-self.global_basis.z) <= -0.1:
+		if !drift_sound:
+			if self.front_wheels[0].is_colliding() and self.front_wheels[1].is_colliding():
+				self.drift_sound = AudioDriver.play_sfx("res://actors/player/sounds/slide.mp3")
+	else:
+		cancelSound(1)
+	
+	if self.desired_thrust * self.linear_velocity.dot(-self.global_basis.z) >= 0.1:
+		if !engine_sound:
+			self.engine_sound = AudioDriver.play_sfx("res://actors/player/sounds/accel.mp3")
+	else:
+		cancelSound(0)
+	
+	self.prev_vel = self.linear_velocity
+
+func cancelSound(which:int):
+	if which:
+		if self.drift_sound:
+			self.drift_sound.stop()
+			self.drift_sound = null
+	else:
+		if self.engine_sound:
+			self.engine_sound.stop()
+			self.engine_sound = null
 
 func _process(delta):
 	self.desired_thrust = Input.get_axis("brake", "gas_pedal")
