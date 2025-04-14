946e8ce5b6a1485a7fbd8fc8d3e006a53b41d669
Harrison Wine <harrisonmwine@protonmail.com>
2025-04-14 19:17:39 -0400

    draft of how collision to be handled

--- actors/shoppers/shopper.gd
+++ actors/shoppers/shopper.gd
@@ -1,4 +1,25 @@
 class_name Shopper extends CharacterBody3D
 
+@onready var sprite_3d: Sprite3D = $Sprite3D
+
+@export var monetaryValueOfLife : int
+@export var representationOfTheSoul : CompressedTexture2D
+
+signal releasedFromMortalCoil
+
+func _ready() -> void:
+	sprite_3d.texture = load("res://actors/shoppers/eldenRingHarrison.png")
+	sprite_3d.billboard = 1
+	
 func _physics_process(delta: float) -> void:
 	move_and_slide()
+
+func _on_collision(area: Area3D) -> void:
+	releasedFromMortalCoil.emit()
+	for collision_index in get_slide_collision_count()-1:
+		var collision = get_slide_collision(collision_index)
+		if collision == area:
+			print(area)
+			var normal = collision.get_normal()
+			# TODO.hw - apply force in direction of colliding object
+	pass # Replace with function body.
