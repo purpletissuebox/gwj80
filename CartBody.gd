65c289a264673bb7bc10a1775e01904b381a08f1
DesktopCoder1407 <>
2025-04-15 20:52:43 -0400

    Fix!: Removed unnecessary files. Simplified collision logic.

--- CartBody.gd
+++
@@ -1,6 +0,0 @@
-class_name CartBodyHitbox
-extends Area3D
-
-func _init() -> void:
-	collision_layer = 2
-	collision_mask = 0
--- levels/Store/ShelfPickup.gd
+++ levels/Store/ShelfPickup.gd
@@ -1,15 +1,4 @@
-class_name ShelfPickup
 extends Area3D
 
-func _init() -> void:
-	collision_layer = 0
-	collision_mask = 2
-
-func _ready() -> void:
-	connect("area_entered", _cart_touch)
-
-func _cart_touch(hitbox: CartBodyHitbox) -> void:
-	if hitbox == null:
-		return
-	else:
-		AudioDriver.play_sfx("res://levels/Store/Sound/You_Know.mp3")
+func _on_shelf_pickup_area_entered(area: Area3D) -> void:
+	AudioDriver.play_sfx("res://levels/Store/Sound/You_Know.mp3")
