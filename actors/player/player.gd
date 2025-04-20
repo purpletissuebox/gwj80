fb2158ab3ae646a15f76cc9ae22f5016212a7309
purple tissue box <dirksenmatthew1@gmail.com>
2025-04-20 13:25:05 -0400

    play engine noise.

--- actors/player/player.gd
+++ actors/player/player.gd
@@ -13,12 +13,31 @@ class_name ShoppingCart extends RigidBody3D
 @export var wheels:Array[RayCast3D]
 
 var engine_power:float
+var engine_sound:AudioStreamPlayer = null
+const engine_sounds:Array[String] = [
+	"res://actors/player/sounds/drive0.mp3",
+	"res://actors/player/sounds/drive1.mp3",
+	"res://actors/player/sounds/drive2.mp3",
+]
 
 func _ready():
+	SignalBus.register_player_cam.emit($Camera3D)
 	var acc = 0
 	for i in 100:
 		acc += self.engine_curve.sample(i/100.0)
 	self.engine_power = (top_speed/zero_to_sixty)/(acc/100)
+
+func _physics_process(delta: float) -> void:
+	var ratio = abs(self.global_basis.x.dot(self.linear_velocity)/self.top_speed)
+	if engine_sound:
+		engine_sound.volume_linear = ratio
+		if ratio <= 0.3:
+			engine_sound.stop()
+			engine_sound = null
+	else:
+		if ratio > 0.3:
+			engine_sound = AudioDriver.play_sfx(engine_sounds.pick_random(), 0)
+	
 
 func apply_force_relative(force:Vector3, lcl_pos:Vector3):
 	var myrotation:Transform3D = self.global_transform
